cb06b02095c249cb72b427a5e5ad1ba21727444d
itsmeberwyn <bherfelismenia@gmail.com>
2022-12-06 12:38:44 +0800

    Game dev for testing

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public AudioSource death;
+    public AudioSource takeDamage;
     public float lifebar;
     // Start is called before the first frame update
     void Start()
@@ -20,9 +22,11 @@ public class Enemy : MonoBehaviour
 
     public int TakeDamage(int damage)
     {
+        takeDamage.Play();
         lifebar -= damage;
         if(lifebar <= 0)
         {
+            death.Play();
             Object.Destroy(this.gameObject);
             return 1;
         }
---
+++ Assets/scripts/KillsUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillsUI : MonoBehaviour
+{
+    public Text kills;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kills.text = "Kills: " + FindObjectOfType<PlayerAttack>().kill;
+    }
+}
